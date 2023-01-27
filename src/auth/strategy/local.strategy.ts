f6852d0907f2873e7cd38f46d99ecb737ac5a75c
류한경 <hangyeong.ryu@gmail.com>
2023-01-27 15:07:45 +0900

    refactor: change passport-local username field

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -7,11 +7,10 @@ import { AuthService } from '../service/auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
+    super({ usernameField: 'email' });
   }
 
   async validate(email: string, password: string): Promise<any> {
-    console.log(email, password);
     const user = await this.authService.validateUser(email, password);
 
     if (!user) throw new UnauthorizedException();
