0304205c02a89fb4fe1385b6f060e29d3033be40
류한경 <hangyeong.ryu@gmail.com>
2023-02-01 21:35:31 +0900

    refactor: add auth sevice test code

--- src/auth/service/auth.service.spec.ts
+++ src/auth/service/auth.service.spec.ts
@@ -1,18 +1,55 @@
+import { ConflictException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
 import { AuthService } from './auth.service';
+import { LoginUserDto } from '../../user/dto/login.user.dto';
+import { UserEntity } from '../../user/entities/user.entity';
+import { UserService } from '../../user/service/user.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let userService: UserService;
+
+  let loginUserDto: LoginUserDto;
+
+  const USER_REPOSITORY_TOKEN = getRepositoryToken(UserEntity);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        UserService,
+        AuthService,
+        {
+          provide: USER_REPOSITORY_TOKEN,
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
+
+    loginUserDto = {
+      email: 'fit-ui@gmail.com',
+      password: 'fit-ui',
+    };
   });
 
-  it.skip('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('login', () => {
+    it('if you do not have a registered email', async () => {
+      jest
+        .spyOn(userService, 'findUserByEmail')
+        .mockReturnValueOnce(Promise.resolve(null) as Promise<UserEntity>);
+
+      await expect(async () => {
+        await authService.login(loginUserDto);
+      }).rejects.toThrowError(new ConflictException('User does not exist'));
+    });
+    // @todo add more tests
   });
 });
